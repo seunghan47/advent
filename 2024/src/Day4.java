895e627f2c136bb19f97b031ef19f64e484c0a02
Paul Lim <paulslim47@gmail.com>
2024-12-04 10:01:43 -0500

    day 4.1

--- 2024/src/Day4.java
+++ 2024/src/Day4.java
@@ -1,2 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Arrays;
+
 public class Day4 {
+    public int answer () {
+        int lines = 0, ans = 0;
+        char [][] grid = new char[140][140];
+        try (BufferedReader br = new BufferedReader(new FileReader("./input4.txt"))){
+            String line;
+            while ((line = br.readLine()) != null){
+                /** filling up the grid */
+                for (int i = 0; i < 140; i++) grid[lines][i] = line.charAt(i);
+                lines++;
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        ans = dfs(grid, 0, 0);
+        return ans;
+    }
+    public int dfs(char[][]grid, int r, int c){
+        return 1;
+    }
 }
--- 2024/src/Main.java
+++ 2024/src/Main.java
@@ -6,9 +6,11 @@ public class Main {
     public static void main(String [] args) {
         Day1 day1 = new Day1();
         Day2 day2 = new Day2();
+        Day4 day4 = new Day4();
 //        day1.answer();
 //        day2.answer();
-        Day3.answer();
+//        Day3.answer();
+        System.out.println(day4.answer());
 //        System.out.println(Main.hi("Hello", new int[]{1, 2, 4}));
     }
 }
