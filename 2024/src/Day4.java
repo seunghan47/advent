b87f8ccfadd121b3db762d613395f8fc8a55db38
Paul Lim <paulslim47@gmail.com>
2024-12-05 17:57:59 -0500

    day 4 - 1

--- 2024/src/Day4.java
+++ 2024/src/Day4.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 
 public class Day4 {
     public int answer () {
+        int [][] directions = {
+                {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {-1, 1}, {-1, -1}, {1, 1}, {1, -1}
+        };
         int lines = 0, ans = 0;
         char [][] grid = new char[140][140];
         try (BufferedReader br = new BufferedReader(new FileReader("./input4.txt"))){
@@ -16,10 +19,21 @@ public class Day4 {
         } catch (Exception e){
             e.printStackTrace();
         }
-        ans = dfs(grid, 0, 0);
+        for (int i = 0; i < grid.length; i++){
+            for (int j = 0; j < grid[i].length; j++){
+                if (grid[i][j] == 'X'){
+                    for (int [] dir : directions) {
+                        ans += dfs(grid, i, j, 0, dir[0], dir[1], "XMAS");
+                    }
+                }
+            }
+        }
         return ans;
     }
-    public int dfs(char[][]grid, int r, int c){
-        return 1;
+    public int dfs(char[][]grid, int r, int c, int index, int dx, int dy, String word) {
+        if (index == 4) return 1;
+        if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] == '.' || grid[r][c] != word.charAt(index))
+            return 0;
+        return dfs(grid, r + dx, c + dy, index + 1, dx, dy, "XMAS");
     }
 }
