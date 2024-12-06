24b1af7531853ba9a8f3db1886861d284fedc3e8
Paul Lim <paulslim47@gmail.com>
2024-12-06 11:31:47 -0500

    day 4-2

--- 2024/src/Day4.java
+++ 2024/src/Day4.java
@@ -10,7 +10,7 @@ public class Day4 {
         int [][] direction = {
                 {1,1}, {-1, -1}, {-1, 1}, {1, -1}
         };
-        int lines = 0, ans = 0;
+        int lines = 0, ans = 0, ans2 = 0;
         char [][] grid = new char[140][140];
         try (BufferedReader br = new BufferedReader(new FileReader("./input4.txt"))){
             String line;
@@ -29,14 +29,41 @@ public class Day4 {
                         ans += dfs(grid, i, j, 0, dir[0], dir[1], "XMAS");
                     }
                 }
+                if (grid[i][j] == 'A'){
+                    boolean forward = dfs2(grid, i, j);
+                    boolean backward = dfs3(grid, i, j);
+                    if (forward && backward) ans2 ++;
+                }
             }
         }
-        return ans;
+//        return ans;
+        return ans2;
     }
     public int dfs(char[][]grid, int r, int c, int index, int dx, int dy, String word) {
         if (index == 4) return 1;
         if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] == '.' || grid[r][c] != word.charAt(index))
             return 0;
         return dfs(grid, r + dx, c + dy, index + 1, dx, dy, "XMAS");
+    }
+    public boolean dfs2(char[][]grid, int r, int c) {
+        boolean containsM = false;
+        boolean containsS = false;
+        if (r - 1 < 0 || c - 1 < 0 || r  >= grid.length - 1 || c >= grid[0].length - 1) return false;
+        if (grid[r - 1][c - 1] == 'M') containsM = true;
+        if (grid[r - 1][c - 1] == 'S') containsS = true;
+        if (grid[r + 1][c + 1] == 'M') containsM = true;
+        if (grid[r + 1][c + 1] == 'S') containsS = true;
+        return (containsM && containsS);
+    }
+    public boolean dfs3(char[][]grid, int r, int c) {
+        boolean containsM = false;
+        boolean containsS = false;
+        if (r - 1 < 0 || c  - 1 < 0 || r >= grid.length - 1 || c >= grid[0].length - 1) return false;
+        if (grid[r - 1][c + 1] == 'M') containsM = true;
+        if (grid[r - 1][c + 1] == 'S') containsS = true;
+        if (grid[r + 1][c - 1] == 'M') containsM = true;
+        if (grid[r + 1][c - 1] == 'S') containsS = true;
+        return (containsM && containsS);
+
     }
 }
--- 2024/src/Main.java
+++ 2024/src/Main.java
@@ -6,12 +6,14 @@ public class Main {
     public static void main(String [] args) {
         Day1 day1 = new Day1();
         Day2 day2 = new Day2();
-//        Day4 day4 = new Day4();
-        Day5 day5 = new Day5();
+        Day4 day4 = new Day4();
+//        Day5 day5 = new Day5();
 //        day1.answer();
 //        day2.answer();
 //        Day3.answer();
-        day5.answer();
+        System.out.println(day4.answer());
+//        day5.answer();
+
 //        System.out.println(Main.hi("Hello", new int[]{1, 2, 4}));
     }
 }
